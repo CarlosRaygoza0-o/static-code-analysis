0c3b37c5d513efbafb58d7e8d9499e521a47b506
CarlosRaygoza0-o <22151153@aguascalientes.tecnm.mx>
2026-05-20 08:47:09 -0600

    Initial commit

---
+++ app/Http/Controllers/OperationsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class OperationsController extends Controller
+{
+    public function addition(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+    
+}
